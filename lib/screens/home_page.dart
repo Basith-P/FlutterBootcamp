a66c20f8affff939afbdf9fd2cc413b2d948ad31
Basith P <basithpofficial@gmail.com>
2021-06-06 15:16:13 +0530

    day4

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,43 +1,82 @@
-import 'package:first_app/screens/widgets/top_bar.dart';
+import 'package:dio/dio.dart';
+import 'package:first_app/models/photos_model.dart';
+import 'package:first_app/widgets/top_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:first_app/env/keys.dart' as config;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PhotosModel> _photosData = [];
+
+  Future<void> _fetchPhotos() async {
+    final _doInstance = Dio();
+
+    _doInstance.options.headers['Authorization'] =
+        "Client-ID ${config.unsplashKey}";
+
+    final _fetchData = await _doInstance.get('https://api.unsplash.com/');
+
+    for (var _items in _fetchData.data) {
+      setState(() {
+        _photosData.add(
+            PhotosModel(id: _items['id'], imgURL: _items['url']['regular']));
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _fetchPhotos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
+        body: SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(),
+        child: SafeArea(
             child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TopBar(
-          title: 'Basith P',
-          subtitle: 'Developer',
-          color: Colors.black,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Text(
-          '  Projects',
-          style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        GridView.builder(
-            padding: EdgeInsets.all(10),
-            itemCount: 4,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 10, mainAxisSpacing: 10, crossAxisCount: 2),
-            itemBuilder: (ctx, index) => Container(
-                  child: Image.network(
-                    'https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                    fit: BoxFit.cover,
-                  ),
-                )),
-      ],
-    )));
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TopBar(
+              title: 'Basith P',
+              subtitle: 'Developer',
+              color: Colors.black,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              '  Projects',
+              style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            GridView.builder(
+                padding: EdgeInsets.all(10),
+                itemCount: _photosData.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2),
+                itemBuilder: (ctx, index) => Container(
+                      child: Image.network(
+                        _photosData[index].imgURL,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+          ],
+        )),
+      ),
+    ));
   }
 }
